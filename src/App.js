c71a63ede31409e850807c928b675634265fdd3c
auster1258 <a935a15@gmail.com>
2020-10-08 17:54:10 +0800

    add one week

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React in One Week
         </a>
       </header>
     </div>
